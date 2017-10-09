a1806e6f7dd37e7bcd4d6cc372721a3600375e1d
zheka <zhen_khokh@mail.ru>
2017-10-09 21:32:58 +0300

    remove double

--- salesman/zone1/Get_Next.m
+++
@@ -1,4 +0,0 @@
-function pos = Get_Next(i,offset,ind)
-i = i+offset;
-n = length(ind);
-pos = ind(1+mod(i-1,n));
--- salesman/zone1/Get_ind.m
+++
@@ -1,3 +0,0 @@
-function i = Get_ind(i,ind)
- n = length(ind);
- i = ind(1+mod(i-1,n));
